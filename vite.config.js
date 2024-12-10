2b20329f792f9e6d05826e94b63d35abd02172af
veronicaagarcia <veroagarcia90@gmail.com>
2024-12-10 14:10:47 -0300

    solucionado

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,29 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import commonjs from 'vite-plugin-commonjs';
+
+
+// export default defineConfig({
+//   plugins: [react({
+//     jsxRuntime: 'automatic', 
+//   }), commonjs()],
+//   esbuild: {
+//     jsx: 'react-jsx', 
+//   },
+//   build: { 
+//     commonjsOptions: { 
+//       include: ['opencv.js'], 
+//       transformMixedEsModules: true,
+//     }, 
+//     rollupOptions: {
+//       external: ['react', 'react-dom'],
+//     },
+//   },
+// })
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import commonjs from 'vite-plugin-commonjs';
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxRuntime: 'automatic', // Importante: Asegúrate de que esta opción esté habilitada
-  }), commonjs()],
-  esbuild: {
-    jsx: 'react-jsx', // Configura JSX para React 18+
-  },
-  build: { 
-    commonjsOptions: { 
-      include: ['opencv.js'], 
-      transformMixedEsModules: true,
-    }, 
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-    },
-  },
+  plugins: [react()],
 })
